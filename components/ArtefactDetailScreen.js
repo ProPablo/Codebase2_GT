e381b686fdcbe94107194aae90cd7930cbf5c3c7
Anhad <anhadrs@gmail.com>
2020-09-11 17:28:31 +1000

    kanye rest test

--- components/ArtefactDetailScreen.js
+++ components/ArtefactDetailScreen.js
@@ -5,13 +5,22 @@ import {
 } from 'react-native';
 
 export default function ArtefactDetailScreen({ route, navigation }) {
-
-    const { Id } = route.params;
+    const [artefact, setartefact] = useState(null);
+    
+    useEffect(() => {
+        const { Id } = route.params;
+        (async ()=> {
+            const apiRes = await fetch("https://api.kanye.rest");
+            const json = await apiRes.json();
+            setartefact(json);
+        })();
+    }, []);
 
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text>Artefact Details!</Text>
             <Text>This is id: {Id}</Text>
+            {artefact && <Text>{JSON.stringify(artefact, null, 2)} </Text>}
         </View>
     )
 }
--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -5,17 +5,6 @@ import {
     Text,
 } from 'react-native';
 
-
-// class HomeScreen extends React.Component {
-//   render() {
-//     return (
-//       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//         <Text>Home Screen</Text>
-//       </View>
-//     )
-//   }
-// }
-
 export default function HomeScreen() {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
