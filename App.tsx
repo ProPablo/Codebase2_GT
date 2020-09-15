0522f26cdea7991d9e0446a62999f5de938506ef
Anhad <anhadrs@gmail.com>
2020-09-15 22:59:50 +1000

    Working logging bluetooth, BUGGED INITIALISER

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 import 'react-native-gesture-handler';
-import React, { useState, useMemo, useEffect } from 'react';
+import React, { useState, useMemo, useEffect, useRef } from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -15,18 +15,44 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {
   View,
   Text,
-  NativeModules
+  PermissionsAndroid,
 } from 'react-native';
 
 
-import ArtefactsScreen from './components/ArtefactsScreen';
 import HomeScreen from './components/HomeScreen';
 import { ArtefactStack } from './components/ArtefactStack';
 import ArtefactsContext, { artefactsContextValue } from './components/ArtefactsContext';
 import { IArtefact, IBaseArtefact } from './lib/Interfaces';
 import { artefactsURL } from './lib/urls';
 
+import { BleManager } from 'react-native-ble-plx';
 
+// const allPerms = [PermissionsAndroid.PERMISSIONS.BLUETOOTH, PermissionsAndroid.PERMISSIONS.BLUETOOTH_ADMIN, PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION];
+const requestLocationPermission = async () => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: 'Location Permission',
+        message:
+          'This example app needs to access your location in order to use bluetooth beacons.',
+        buttonNeutral: 'Ask Me Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      console.log("GOT PERMS")
+      return true;
+    } else {
+      // permission denied
+      return false;
+    }
+  } catch (err) {
+    console.warn(err);
+    return false;
+  }
+};
 
 
 class EventsScreen extends React.Component {
@@ -69,6 +95,8 @@ function Tabs() {
 export default function App() {
   const [artefacts, setArtefacts] = useState<IArtefact[]>([]);
 
+  const manager = useRef<BleManager | null>(null);
+
   async function getArtefacts(): Promise<IArtefact[]> {
     let json;
     try {
@@ -78,7 +106,6 @@ export default function App() {
     } catch (error) {
       console.error("ERROR RETREIVING ARTEFACTS");
     }
-
     return json;
   }
 
@@ -86,9 +113,36 @@ export default function App() {
     setArtefacts(await getArtefacts());
   }
 
+  const scanAndConnect = () => {
+    manager.current?.startDeviceScan(null, null, (error, device) => {
+      console.log({ device });
+    });
+  }
+
   useEffect(() => {
     loadArtefacts();
-    console.log("Logging artefacts", artefacts);
+
+    requestLocationPermission()
+      .then(() => {
+        manager.current = new BleManager();
+        manager.current?.startDeviceScan(null, null, (error, device) => {
+          console.log({ device });
+        });
+
+        // const subscription = manager.current.onStateChange((state => {
+        //   console.log("BLE Manager online");
+        //   if (state === 'PoweredOn') {
+        //     console.log("Starting scanning");
+        //     scanAndConnect();
+        //     subscription.remove();
+        //   }
+        // }));
+      })
+    return () => {
+      // manager.current?.stopDeviceScan();
+      // manager.current?.destroy();
+    }
+
   }, []);
 
   const providerValue = useMemo(() => ({ artefacts, loadArtefacts }), [artefacts, loadArtefacts]); //Only recomputes as object when logintoken or setLogintoken change
